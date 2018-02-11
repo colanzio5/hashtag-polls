47c44e8cd2c95cd35d96ea2daa4d80f79ac4b37a
Colin Casazza <colanzio5@gmail.com>
2018-02-11 15:14:19 -0800

    commits

--- bin/chron-script.js
+++ bin/chron-script.js
@@ -28,127 +28,128 @@ function script() {
             console.log(e)
         }
     });
-}
 
-function query_campaigns() {
-    //run immediately 
-    Campaign.getCampaigns((err, campaigns) => {
-        if (err)
-            console.log(err);
-        let now = new Date();
-        campaigns.forEach(campaign => {
-            //here we determin if a campaign has reached a tweet limit or date limit
-            if ( typeof campaign.max_tweets !== 'undefined' && campaign.max_tweets ){
-                if(campaign.max_tweets <= campaign.number_tweets){
-                    console.log("too many tweets! " + campaign.campaign_name);
-                    return;
+    function query_campaigns() {
+        //run immediately 
+        Campaign.getCampaigns((err, campaigns) => {
+            if (err)
+                console.log(err);
+            let now = new Date();
+            campaigns.forEach(campaign => {
+    
+                //here we determin if a campaign has reached a tweet limit or date limit
+                // return if expired or invalid
+                if ( typeof campaign.max_tweets !== 'undefined' && campaign.max_tweets ){
+                    if(campaign.max_tweets <= campaign.number_tweets) return;
                 }
-            }
-            if ( typeof campaign.start_date !== 'undefined' && campaign.start_date ){
-                if(campaign.start_date >= now) {
-                    console.log("campaign has not started "+ campaign.campaign_name);
-                    return;
+                if ( typeof campaign.start_date !== 'undefined' && campaign.start_date ){
+                    if(campaign.start_date >= now) return;
                 }
-            }
-            if ( typeof campaign.end_date !== 'undefined' && campaign.end_date ){
-                if(campaign.end_date <= now){ 
-                    console.log("campaign has ended "+ campaign.campaign_name);
-                    return;    
+                if ( typeof campaign.end_date !== 'undefined' && campaign.end_date ){
+                    if(campaign.end_date <= now) return;
                 }
-            }
-            console.log("no invalid limits found, running search on " + campaign.campaign_name)
-            console.log("---end campaign---\n\n\n");
-
-            Tweet.getTweetByCampaignID(campaign._id, (err, tweet) => {
-                if (err) throw err;
-                //check if campaign has matching tweet object in database
-                if (tweet === null) {
-                    //create new db tweetlist entry if no entry
-                    let new_tweet = {
-                        "_id": campaign._id,
-                        "tweets": []
-                    };
-                    Tweet.addTweet(new_tweet, (err, tweet) => {
-                        if (err) {
-                            throw err;
+    
+                //if not invalid continue with search
+                Tweet.getTweetByCampaignID(campaign._id, (err, tweet) => {
+                    if (err) throw err;
+                    
+                    //check if campaign has matching tweet object in database
+                    if (tweet === null) {
+                        //create new db tweetlist entry if no entry
+                        let new_tweet = {
+                            "_id": campaign._id,
+                            "tweets": []
+                        };
+                        Tweet.addTweet(new_tweet, (err, tweet) => {
+                            if (err) throw err;
+                            try {
+                                main_search(campaign, tweet);
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        });
+                    } else {
+                        //otherwise campaign has tweet-list object, use likewise
+                        try {
+                            main_search(campaign, tweet);
+                        } catch (err) {
+                            console.log(err);
                         }
-                        console.log("New Tweet Object Added: " + new_tweet._id);
-                        //run search on new tweet object
-                        main_search(campaign, tweet);
-                    });
-                } else {
-                    //current tweet object found, run search on existing tweet object
-                    main_search(campaign, tweet);
-                }
-            });
-        });
-    });
-}
-
-function current_campaign(campaign, tweet_list){
-    console.log('search ran');
-}
-
-function main_search(campaign, tweet_list) {
-    console.log("(Campaign ID, Tweet ID)::( " + campaign._id + " || " + tweet_list._id + " )");
-    let current_tweet_ids = tweet_list.tweets.map(tweet => {
-        return tweet.id_str
-    });
-
-    T.get('search/tweets', {
-        q: campaign.campaign_tags
-    }, function (err, data, response) {
-        if (err) throw err;
-        data.statuses.forEach(tweet => {
-            if (!current_tweet_ids.includes(tweet.id_str)) {
-                let new_tweet = {
-                    contributors: tweet.contributors,
-                    created_at: tweet.created_at,
-                    favorited: tweet.favorited,
-                    geo: tweet.geo,
-                    id: tweet.id,
-                    id_str: tweet.id_str,
-                    in_reply_to_screen_name: tweet.in_reply_to_screen_name,
-                    in_reply_to_status_id: tweet.in_reply_to_status_id,
-                    in_reply_to_status_id_str: tweet.in_reply_to_status_id_str,
-                    in_reply_to_user_id: tweet.in_reply_to_user_id,
-                    in_reply_to_user_id_str: tweet.in_reply_to_user_id_str,
-                    retweet_count: tweet.retweet_count,
-                    retweeted: tweet.retweeted,
-                    source: tweet.source,
-                    text: tweet.text,
-                    truncated: tweet.truncated,
-                    user_id_str: tweet.user.id_str,
-                    user_lang: tweet.user.lang,
-                    user_location: tweet.user.location,
-                    user_name: tweet.user.name,
-                    user_profile_background_image_url: tweet.user.user_profile_background_image_url,
-                    user_profile_image_url: tweet.user.profile_image_url,
-                    user_screen_name: tweet.user.screen_name,
-                    user_url: tweet.user.url,
-                }
-                Tweet.findByIdAndUpdate(tweet_list._id, {
-                    "$push": {
-                        "tweets": new_tweet
                     }
-                }, {
-                    "new": true,
-                    "upsert": true
-                }, (err, res) => {
-                    if (err) console.log("Tweet Casting Error")
                 });
-            }
-        });
-        //all new tweets pulled in, update analytics here
-        Tweet.findById(campaign._id, (err, tweet) => {
-            console.log("Update to length: " + tweet.tweets.length);
-            Campaign.findByIdAndUpdate(campaign._id, {
-                $set: {
-                    "number_tweets": tweet.tweets.length
-                }
-            }, (err, res) => {
-                if (err) console.log(err);
             });
         });
-    });
+    }
+    
+    function main_search(campaign, tweet_list) {
+    
+        //get list of tweets currently associated with campaign
+        let current_tweet_ids = tweet_list.tweets.map(tweet => {
+            return tweet.id_str
+        });
+    
+        //run a search using the campaign's tags
+        T.get('search/tweets', {
+            q: campaign.campaign_tags
+        }, function (err, data, response) {
+            if (err) throw err;
+    
+            //cycle through list of tweets returned from search,
+            //add new tweets (not listed in current_tweet_ids) to campaign's tweet list
+            data.statuses.forEach(tweet => {
+                if (!current_tweet_ids.includes(tweet.id_str)) {
+                    let new_tweet = {
+                        contributors: tweet.contributors,
+                        created_at: tweet.created_at,
+                        favorited: tweet.favorited,
+                        geo: tweet.geo,
+                        id: tweet.id,
+                        id_str: tweet.id_str,
+                        in_reply_to_screen_name: tweet.in_reply_to_screen_name,
+                        in_reply_to_status_id: tweet.in_reply_to_status_id,
+                        in_reply_to_status_id_str: tweet.in_reply_to_status_id_str,
+                        in_reply_to_user_id: tweet.in_reply_to_user_id,
+                        in_reply_to_user_id_str: tweet.in_reply_to_user_id_str,
+                        retweet_count: tweet.retweet_count,
+                        retweeted: tweet.retweeted,
+                        source: tweet.source,
+                        text: tweet.text,
+                        truncated: tweet.truncated,
+                        user_id_str: tweet.user.id_str,
+                        user_lang: tweet.user.lang,
+                        user_location: tweet.user.location,
+                        user_name: tweet.user.name,
+                        user_profile_background_image_url: tweet.user.user_profile_background_image_url,
+                        user_profile_image_url: tweet.user.profile_image_url,
+                        user_screen_name: tweet.user.screen_name,
+                        user_url: tweet.user.url,
+                    }
+                    Tweet.findByIdAndUpdate(tweet_list._id, {
+                        "$push": {
+                            "tweets": new_tweet
+                        }
+                    }, {
+                        "new": true,
+                        "upsert": true
+                    }, (err, res) => {
+                        if (err) throw err;
+                    });
+                }
+            });
+    
+            //all new tweets pulled in, update analytics here
+            Tweet.findById(campaign._id, (err, tweet) => {
+                Campaign.findByIdAndUpdate(campaign._id, {
+                    $set: {
+    
+                        //length of tweets array after new tweets added
+                        "number_tweets": tweet.tweets.length
+                    }
+                }, (err, res) => {
+                    if (err) throw err;
+                });
+            });
+        });
+    }
 }
+
