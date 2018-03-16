0062b5daa9d051b7fa55a9f1679a4343baa04369
Colin Casazza <colanzio5@gmail.com>
2018-03-15 17:05:48 -0700

    added sentiment and word count visualization

--- client/app.js
+++ client/app.js
@@ -1,27 +1,27 @@
-var myApp = angular.module('tweet-tracker',['ngRoute', 'angularUtils.directives.dirPagination']);
+var myApp = angular.module('tweet-tracker',['ngRoute', 'angularUtils.directives.dirPagination', 'nvd3']);
 
 myApp.config(function($routeProvider){
-	$routeProvider.when('/', {
-		controller:'CampaignsController',
-		templateUrl: 'views/campaigns.html'
-	})
-	.when('/campaigns', {
-		controller:'CampaignsController',
-		templateUrl: 'views/campaigns.html'
-	})
-	.when('/campaigns/details/:id',{
-		controller:'CampaignsController',
-		templateUrl: 'views/campaign_details.html'
-	})
-	.when('/campaigns/add',{
-		controller:'CampaignsController',
-		templateUrl: 'views/add_campaign.html'
-	})
-	.when('/campaigns/edit/:id',{
-		controller:'CampaignsController',
-		templateUrl: 'views/edit_campaign.html'
-	})
-	.otherwise({
-		redirectTo: '/'
+		$routeProvider.when('/', {
+			controller:'CampaignsController',
+			templateUrl: 'views/campaigns.html'
+		})
+		.when('/campaigns', {
+			controller:'CampaignsController',
+			templateUrl: 'views/campaigns.html'
+		})
+		.when('/campaigns/details/:id',{
+			controller:'CampaignsController',
+			templateUrl: 'views/campaign_details.html'
+		})
+		.when('/campaigns/add',{
+			controller:'CampaignsController',
+			templateUrl: 'views/add_campaign.html'
+		})
+		.when('/campaigns/edit/:id',{
+			controller:'CampaignsController',
+			templateUrl: 'views/edit_campaign.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
 	});
-});
--- client/controllers/campaigns.js
+++ client/controllers/campaigns.js
@@ -1,73 +1,103 @@
 var myApp = angular.module('tweet-tracker');
 
-myApp.controller('CampaignsController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+myApp
+	.controller('CampaignsController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+		$scope.getCampaigns = function () {
+			$http.get('/api/campaigns').success(function (response) {
+				$scope.campaigns = response;
+			});
+		};
 
-	$scope.getCampaigns = function () {
-		$http.get('/api/campaigns').success(function (response) {
-			$scope.campaigns = response;
-		});
-	}
+		$scope.pagination = {
+			current: 1
+		};
 
-	$scope.pagination = {
-        current: 1
-    };
+		$scope.pageChanged = function (newPage) {
+			refreshTweets(newPage);
+		};
 
-    $scope.pageChanged = function(newPage) {
-		refreshTweets(newPage);
-	};
-	
-	function refreshTweets(pageNumber){
-		$http.get('/api/tweets/' + $scope.id + '/' + pageNumber).success(function (response) {
-			$scope.tweets = response;
-		})
-	}
+		function refreshTweets(pageNumber) {
+			$http.get('/api/tweets/' + $scope.id + '/' + pageNumber).success(function (response) {
+				$scope.tweets = response;
+			})
+		};
 
-	$scope.getCampaign = function () {
-		let id = $routeParams.id;
-		$http.get('/api/campaigns/' + id).success(function (response) {
-			$scope.campaign = response;
-			$scope.id = id;
-			$scope.number_tweets = $scope.campaign.number_tweets;
-			$scope.now = new Date();
-		});
-	}
+		$scope.getCampaign = function () {
+			let id = $routeParams.id;
+			$http.get('/api/campaigns/' + id).success(function (response) {
+				$scope.campaign = response;
+				$scope.id = id;
+				$scope.number_tweets = $scope.campaign.number_tweets;
+				$scope.now = new Date();
+				console.log($scope.campaign.frequent_words);
 
-	$scope.getTweets = function (page) {
-		let id = $routeParams.id;
+				$scope.o1 = {
+					chart: {
+						type: 'multiBarHorizontalChart',
+						height: 2000,
+						width: 500,
+						x: function(d){return d.word;},
+						y: function(d){return d.count;},
+						//yErr: function(d){ return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] },
+						showControls: true,
+						showValues: true,
+						duration: 500,
+						xAxis: {
+							showMaxMin: false
+						},
+						yAxis: {
+							axisLabel: 'Values',
+							tickFormat: function(d){
+								return d3.format(',f')(d);
+							}
+						}
+					}
+				};
+		
+				$scope.d1 = [{
+					key: "Cumulative Return",
+					values: $scope.campaign.frequent_words
+				}]
+			});			
+		};
 
-		$http.get('/api/tweets/' + id + '/' + page).success(function (response) {
-			$scope.tweets = response;
-		})
-	}
+		$scope.getTweets = function (page) {
+			let id = $routeParams.id;
 
-	$scope.export_tweets = function () {
-		var s = $scope.tweets
-		var jsons = [];
-		for (var i = s.length; i--;) jsons[i] = JSON.stringify(s[i]);
-		var blob = new Blob(jsons, {
-			type: "text/plain;charset=utf-8"
-		});
-		saveAs(blob, "tweet_list.txt");
-	}
+			$http.get('/api/tweets/' + id + '/' + page).success(function (response) {
+				$scope.tweets = response;
+			})
+		};
 
-	$scope.addCampaign = function () {
-		$http.post('/api/campaigns/', $scope.campaign).success(function (response) {
-			window.location.href = '#/campaigns';
-		});
-	}
+		$scope.export_tweets = function () {
+			var s = $scope.tweets
+			var jsons = [];
+			for (var i = s.length; i--;) jsons[i] = JSON.stringify(s[i]);
+			var blob = new Blob(jsons, {
+				type: "text/plain;charset=utf-8"
+			});
+			saveAs(blob, "tweet_list.txt");
+		};
 
-	$scope.updateCampaign = function () {
-		var id = $routeParams.id;
-		$http.put('/api/campaigns/' + id, $scope.campaign).success(function (response) {
-			window.location.href = '#/campaigns';
-		});
-	}
+		$scope.addCampaign = function () {
+			$http.post('/api/campaigns/', $scope.campaign).success(function (response) {
+				window.location.href = '#/campaigns';
+			});
+		};
 
-	$scope.removeCampaign = function (id) {
-		$http.delete('/api/campaigns/' + id).success(function (response) {});
+		$scope.updateCampaign = function () {
+			var id = $routeParams.id;
+			$http.put('/api/campaigns/' + id, $scope.campaign).success(function (response) {
+				window.location.href = '#/campaigns';
+			});
+		};
 
-		$http.delete('/api/tweets/' + id).success(function (response) {
-			window.location.href = '#/campaigns';
-		});
-	}
-}]);
+		$scope.removeCampaign = function (id) {
+			$http.delete('/api/campaigns/' + id).success(function (response) {});
+
+			$http.delete('/api/tweets/' + id).success(function (response) {
+				window.location.href = '#/campaigns';
+			});
+		};
+
+	}]);
